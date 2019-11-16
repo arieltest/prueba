3296348defd97067c0f7da4c5ef29faade9a8bb4
ariel zapata <superarichaka@hotmail.com>
2019-11-16 12:19:30 -0600

    nuevo palindromo
    correccion
    
    nuevo comentario
    
    nuevo archivo
    
    hola
    
    nuevo cambio
    
    cambio2
    
    un cambio nuevo
    
    un nuevo cambio 2
    
    otro cambio 3

--- ss.rb
+++ ss.rb
@@ -3,10 +3,18 @@ def palindrome?(words)
   word = word.delete(' ')
   word = = word.reverse
 end
-
+asdasdadadasdsd
+ascascascac
+asdasd
 palindrome?("anita lava la tina")
 Agregando faltante :D
 nueva linea
 nueva linea2
 nueva linea3
 nueva linea 4
+
+nuevo cambio ac
+
+cambio nuevo2
+cambio nuevo3
+cambio asskdjnaksdjn
